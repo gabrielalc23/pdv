7cb261720fd8033b1af464817a52c7121651540e
Gabriel Campos <gabriellopes.campos23@gmail.com>
2026-07-16 19:05:58 -0300

    remove(ui-kit): drop tests and test-only dependencies

---
+++ frontend/packages/ui-kit/eslint.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "eslint/config"
+import { createWorkspaceEslintConfig } from "@pdv/eslint"
+
+export default defineConfig([
+  ...createWorkspaceEslintConfig({
+    tsconfigRootDir: import.meta.dirname,
+    tsconfigProject: ["./tsconfig.app.json", "./tsconfig.node.json"],
+  }),
+])
